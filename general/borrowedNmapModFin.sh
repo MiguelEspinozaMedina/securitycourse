a461c4cd2325697d82107c34f30bf5a422021752
whitehack <securecap@gmail.com>
2015-10-02 07:58:15 -0400

    modified scripts

--- general/borrowedNmapModFin.sh
+++ general/borrowedNmapModFin.sh
@@ -28,7 +28,7 @@ fi
 
 ################### Create a loop of the various nmap scans to perform ##############################
 declare -a nmapSwitches=('-sV -p 20,21,22 --open --script ftp-anon.nse'
-	    '-T5 -PN -p 445 -sS -n --min-hostgroup 8192 --min-rtt-timeout 1000 --min-parallelism 4096 --script nbstat.nse'
+	    '-T5 -PN -p 445 -sS -n --min-hostgroup 8192 --min-rtt-timeout 1000ms --min-parallelism 4096 --script nbstat.nse'
             '-sV -p 5800,5801,5802,5803,5900,5901,5902,5903 --open --script vnc-info.nse'
             '-sV -p 5800,5801,5802,5803,5900,5901,5902,5903 --open --script realvnc-auth-bypass.nse'
             '-p 69 -sU --open --script tftp-enum.nse'
@@ -38,18 +38,7 @@ declare -a nmapSwitches=('-sV -p 20,21,22 --open --script ftp-anon.nse'
             '--script smb-check-vulns -p 139,445 --script-args unsafe=1'
             '--script smb-enum-shares.nse --script-args smbdomain=domain,smbuser=user,smbpass=password -p 445'
             '--script smtp-enum-users.nse -p 25,465,587'
-            '--script smtp-vuln-cve2011-1764.nse -p 25,465,587'
-	    '--script smb-security-mode.nse -p 139,445'
-	    '--script ftp-vuln-cve2010-4221.nse -p 21'
-	    '--script mysql-vuln-cve2012-2122.nse -p 3306'
-	    '--script rmi-vuln-classloader.nse -p 1099'
-	    '--script nmap-Rdp-vuln-ms12-020.nse -p 3389'
-	    '--script afp-path-vuln.nse'
-	    '--script http-vuln-cve2012-1823.nse'
-	    '--script http-vuln-cve2013-0156.nse'
-            '--script http-iis-webdav-vuln.nse'
-	    '--script http-vmware-path-vuln.nse -p 80,443,8222,8333'
-	    '--script http-vuln-zimbra-lfi.nse');
+	    '--script smb-security-mode.nse -p 139,445');
 declare -a typeOfScan=('nmap-sV-FTP'
 	    'nmap-Nbstat'
             'nmap-sV-VNC'
@@ -61,18 +50,7 @@ declare -a typeOfScan=('nmap-sV-FTP'
             'nmap-Samba-check-vulns'
             'nmap-Samba-enum-shares'
             'nmap-Smtp-enum-users'
-            'nmap-Smtp-vuln-cve2011-1764'
-	    'nmap-Smb-security-mode'
-	    'nmap-Ftp-vuln-cve2010-4221'
-	    'nmap-Mysql-vuln-cve2012-2122'
-	    'nmap-Rmi-vuln-classloader'
-	    'nmap-Rdp-vuln-ms12-020'
-	    'nmap-Afp-path-vuln'
-	    'nmap-Http-vuln-cve2012-1823'
-	    'nmap-Http-vuln-cve2013-0156'
-	    'nmap-Http-iis-webdav-vuln'
-	    'nmap-Http-vmware-path-vuln'
-	    'nmap-Http-vuln-zimbra-lfi');
+	    'nmap-Smb-security-mode');
 
 echo "The nmap scripts scans started" >> elapsedTime.txt
 date >> elapsedTime.txt
